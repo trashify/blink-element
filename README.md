60c7ce887e8103cdba07a829883d51e4c912b49c
Tiaan <tiaanduplessis@hotmail.com>
2017-03-14 13:36:33 +0200

    :tada: v1.0.1

--- README.md
+++ README.md
@@ -1,23 +1,72 @@
-<p align="center">
-	<a href="https://tiaanduplessis.github.io/blink-element/"><img width="75%" src="media/blink.gif" alt="Blink - Simple module to make DOM element blink"></a>
-</p>
+<h1 align="center">✨ blink-element</h1>
+<br>
+<div align="center">
+	<img width="100%" src="https://raw.githubusercontent.com/tiaanduplessis/blink-element/master/media/blink.gif" alt="Blink - For those times you wanna make a element blink using JS" />
+</div>
+<br>
+<div align="center">
+  <a href="https://npmjs.org/package/blink-element">
+    <img src="https://img.shields.io/npm/v/blink-element.svg?style=flat-square" alt="Package version" />
+  </a>
+  <a href="https://npmjs.org/package/blink-element">
+  <img src="https://img.shields.io/npm/dm/blink-element.svg?style=flat-square" alt="Downloads" />
+  </a>
+  <a href="https://github.com/feross/standard">
+    <img src="https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square" alt="Standard" />
+  </a>
+  <a href="https://travis-ci.org/tiaanduplessis/blink-element">
+    <img src="https://img.shields.io/travis/tiaanduplessis/blink-element.svg?style=flat-square" alt="Travis Build" />
+  </a>
+  <a href="https://github.com/RichardLitt/standard-readme)">
+    <img src="https://img.shields.io/badge/standard--readme-OK-green.svg?style=flat-square" alt="Standard Readme" />
+  </a>
+  <a href="https://badge.fury.io/gh/tiaanduplessis%2Fblink-element">
+    <img src="https://badge.fury.io/gh/tiaanduplessis%2Fblink-element.svg?style=flat-square" alt="GitHub version" />
+  </a>
+  <a href="https://dependencyci.com/github/tiaanduplessis/blink-element">
+    <img src="https://dependencyci.com/github/tiaanduplessis/blink-element/badge?style=flat-square" alt="Dependency CI" />
+  </a>
+  <a href="https://github.com/tiaanduplessis/blink-element/blob/master/other/LICENSE">
+    <img src="https://img.shields.io/npm/l/blink-element.svg?style=flat-square" alt="License" />
+  </a>
+  <a href="http://makeapullrequest.com">
+    <img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square" alt="PRs" />
+  </a>
+  <a href="https://www.paypal.me/tiaanduplessis/1">
+    <img src="https://img.shields.io/badge/$-support-green.svg?style=flat-square" alt="Donate" />
+  </a>
+</div>
+<br>
+<div align="center">
+  <a href="https://github.com/tiaanduplessis/blink-element/watchers">
+    <img src="https://img.shields.io/github/watchers/tiaanduplessis/blink-element.svg?style=social" alt="Github Watch Badge" />
+  </a>
+  <a href="https://github.com/tiaanduplessis/blink-element/stargazers">
+    <img src="https://img.shields.io/github/stars/tiaanduplessis/blink-element.svg?style=social" alt="Github Star Badge" />
+  </a>
+  <a href="https://twitter.com/intent/tweet?text=Check%20out%20blink-element!%20https://github.com/tiaanduplessis/blink-element%20%F0%9F%91%8D">
+    <img src="https://img.shields.io/twitter/url/https/github.com/tiaanduplessis/blink-element.svg?style=social" alt="Tweet" />
+  </a>
+</div>
+<br>
+<div align="center">
+  Built with ❤︎ by <a href="tiaanduplessis.co.za">Tiaan</a> and <a href="https://github.com/tiaanduplessis/blink-element/graphs/contributors">contributors</a>
+</div>
 
+<h2>Table of Contents</h2>
+<details>
+  <summary>Table of Contents</summary>
+	<li><a href="#about">about</a></li>
+  <li><a href="#install">Install</a></li>
+  <li><a href="#usage">Usage</a></li>
+  <li><a href="#api">API</a></li>
+  <li><a href="#contribute">Contribute</a></li>
+  <li><a href="#license">License</a></li>
+</details>
 
-# blink-element 
-[![Standard - JavaScript Style Guide](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
-[![Build Status](https://travis-ci.org/tiaanduplessis/blink-element.svg?branch=master)](https://travis-ci.org/tiaanduplessis/blink-element)
-[![standard-readme compliant](https://img.shields.io/badge/standard--readme-OK-green.svg?style=flat-square)](https://github.com/RichardLitt/standard-readme)
+## About
 
-> Simple module to make DOM element blink
-
-## Table of Contents
-
-- [Install](#install)
-- [Usage](#usage)
-- [API](#api)
-- [Contribute](#contribute)
-- [License](#license)
-
+A small module to make a DOM element blink using JS.
 
 ## Install
 
@@ -59,7 +108,7 @@ blink({
 
 ## API
 
-The `blink` function takes a single object as its argument with `element` and `speed` as its properties. 
+The `blink` function takes a single object as its argument with `element` and `speed` as its properties.
 
 See [usage](#usage) for an example of how to call the function.
 
@@ -68,8 +117,9 @@ See [usage](#usage) for an example of how to call the function.
 
 Contributions are welcome. Please open up an issue or create PR if you would like to help out.
 
-Small note: If editing the README, please conform to the [standard-readme](https://github.com/RichardLitt/standard-readme) specification.
+Note: If editing the README, please conform to the [standard-readme](https://github.com/RichardLitt/standard-readme) specification.
 
 ## License
 
-Created with ♥ by [Tiaan](http://tiaanduplessis.co.za). Licensed under the MIT License.
+Licensed under the MIT License.
+
--- demo/blink.js
+++ demo/blink.js
@@ -1,81 +1,81 @@
 /**
  *
  * @name blink-element
- * @version 1.0.0
- * @author Tiaan du Plessis (tiaanduplessis@hotmail.com)
+ * @version 1.0.1
+ * @author Tiaan du Plessis
  * @license MIT
  */
 
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.blink = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+(function (f) { if (typeof exports === 'object' && typeof module !== 'undefined') { module.exports = f() } else if (typeof define === 'function' && define.amd) { define([], f) } else { var g; if (typeof window !== 'undefined') { g = window } else if (typeof global !== 'undefined') { g = global } else if (typeof self !== 'undefined') { g = self } else { g = this }g.blink = f() } })(function () {
+  var define, module, exports; return (function e (t, n, r) { function s (o, u) { if (!n[o]) { if (!t[o]) { var a = typeof require === 'function' && require; if (!u && a) return a(o, !0); if (i) return i(o, !0); var f = new Error("Cannot find module '" + o + "'"); throw f.code = 'MODULE_NOT_FOUND', f } var l = n[o] = {exports: {}}; t[o][0].call(l.exports, function (e) { var n = t[o][1][e]; return s(n || e) }, l, l.exports, e, t, n, r) } return n[o].exports } var i = typeof require === 'function' && require; for (var o = 0; o < r.length; o++)s(r[o]); return s })({1: [function (require, module, exports) {
+    'use strict'
 
 /**
  * Lets a element blink based on the speed provided
  */
 
-module.exports = function blink() {
-  var args = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { speed: 0.01 };
+    module.exports = function blink () {
+      var args = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { speed: 0.01 }
 
-  if (!args.element) {
-    throw new Error('No element provided');
-  }
+      if (!args.element) {
+        throw new Error('No element provided')
+      }
 
-  var isStopped = false;
-  args.element.style.opacity = 1;
+      var isStopped = false
+      args.element.style.opacity = 1
 
-  var loop = function loop() {
-    if (!isStopped) {
-      fadeIn(args.element, args.speed, function () {
-        fadeOut(args.element, args.speed, function () {
-          return loop();
-        });
-      });
+      var loop = function loop () {
+        if (!isStopped) {
+          fadeIn(args.element, args.speed, function () {
+            fadeOut(args.element, args.speed, function () {
+              return loop()
+            })
+          })
+        }
+      }
+
+      loop()
+
+      return {
+        start: function start () {
+          isStopped = false
+          loop()
+        },
+        stop: function stop () {
+          isStopped = true
+        }
+      }
     }
-  };
-
-  loop();
-
-  return {
-    start: function start() {
-      isStopped = false;
-      loop();
-    },
-    stop: function stop() {
-      isStopped = true;
-    }
-  };
-};
 
 /**
  * Get requestAnimationFrame for env, otherwise use setTimeout
  */
-var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (cb) {
-  window.setTimeout(cb, 1000 / 60);
-};
+    var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (cb) {
+      window.setTimeout(cb, 1000 / 60)
+    }
 
 /**
  * Peform fade out animation on element based on speed
  */
-function fadeOut(elem, speed, cb) {
-  elem.style.opacity = 1;
-  var fade = function fade() {
-    elem.style.opacity -= speed;
-    elem.style.opacity < 0 ? cb() : raf(fade);
-  };
-  fade();
-}
+    function fadeOut (elem, speed, cb) {
+      elem.style.opacity = 1
+      var fade = function fade () {
+        elem.style.opacity -= speed
+        elem.style.opacity < 0 ? cb() : raf(fade)
+      }
+      fade()
+    }
 
 /**
  * Peform fade in animation on element based on speed
  */
-function fadeIn(elem, speed, cb) {
-  elem.style.opacity = 0;
-  var fade = function fade() {
-    elem.style.opacity = parseFloat(elem.style.opacity) + speed;
-    elem.style.opacity > 1 ? cb() : raf(fade);
-  };
-  fade();
-}
-
-},{}]},{},[1])(1)
-});
+    function fadeIn (elem, speed, cb) {
+      elem.style.opacity = 0
+      var fade = function fade () {
+        elem.style.opacity = parseFloat(elem.style.opacity) + speed
+        elem.style.opacity > 1 ? cb() : raf(fade)
+      }
+      fade()
+    }
+  }, {}]}, {}, [1])(1)
+})
